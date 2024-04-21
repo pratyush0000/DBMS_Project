1b575a47f3710c3535bb43c4c1693d0a3286dfc6
Pratyush Kamal <pratyushkamal88@gmail.com>
2024-04-21 18:55:52 +0530

    update

--- hos_client/src/components/DoctorHome.jsx
+++ hos_client/src/components/DoctorHome.jsx
@@ -1,31 +1,28 @@
 import React from 'react';
-import styles from './DoctorHome.module.css';
+import styles from './DoctorHome.module.css'; // Import CSS module
 import axios from 'axios';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const DoctorHome = () => {
+  const instance = axios.create({
+    baseURL: 'http://localhost:8080'
+  });
 
-      const instance = axios.create({
-        baseURL: 'http://localhost:8080'
-      });
+  const [isModalOpen1, setIsModalOpen1] = useState(false);
+  const [isAddModalOpen, setIsAddModalOpen] = useState(false);
+  const [doctorid, setDoctorid] = useState(0);
+  const [prescription, setPrescription] = useState([]);
+  const [diagnosis, setDiagnosis] = useState("");
+  const [advice, setAdvice] = useState("");
+  const [consultantNotes, setConsultantNotes] = useState("");
+  const [presIdToUpdate, setPresIdToUpdate] = useState(0);
+  const [meds, setMeds] = useState([]);
+  const [currentMed, setCurrentMed] = useState({ name: '', noofdays: '', frequency: '' });
+  const [isFormVisible, setIsFormVisible] = useState(false);
 
-const [isModalOpen1, setIsModalOpen1] = useState(false);
-const [isAddModalOpen, setIsAddModalOpen] = useState(false);
-const [doctorid,setdoctorid] = useState(0);
-const [prescription,setprescription] = useState([]);
-const [diagnosis,setdiagnosis] = useState("");
-const [advice,setadvice] = useState("");
-const [consultant_notes,setconsultant_notes] = useState("");
-const [presIdToUpdate, setPresIdToUpdate] = useState(0);
-const [meds, setMeds] = useState([]);
-const [currentMed, setCurrentMed] = useState({ name: '', noofdays: '', frequency: '' });
-const [isFormVisible, setIsFormVisible] = useState(false);
   const openModal1 = () => {
     setIsModalOpen1(true);
-
-   
-    
   };
 
   const closeModal1 = () => {
@@ -42,174 +39,169 @@ const [isFormVisible, setIsFormVisible] = useState(false);
     setIsAddModalOpen(false);
   }
 
-const getappointments=async ()=>{
+  const getappointments = async () => {
     console.log(doctorid);
-  const response = await instance.get('/api/docprescriptions', {
-    params: {
+    const response = await instance.get('/api/docprescriptions', {
+      params: {
         doctorid: doctorid
-    }
-  });
-  console.log(response);
-  setprescription(response.data);
-  return response.data; 
+      }
+    });
+    console.log(response);
+    setPrescription(response.data);
+    return response.data;
   }
+
+  const submitUpdatedRecords = async (event, presid) => {
+    event.preventDefault();
+    console.log(diagnosis, advice, consultantNotes, doctorid, presid);
+
+    const response = await instance.post('/api/updateprescription', {
+      status: 1,
+      diagnosis: diagnosis,
+      advice: advice,
+      consultant_notes: consultantNotes,
+      doctorid: doctorid,
+      presid: presid,
+      meds: meds
+    });
     
+    console.log(response);
+    closeModal1();
+    closeAddModal();
+  }
 
-const submitupdatedrecords = async (event,presid) => {
-  event.preventDefault();
-  console.log(diagnosis);
-  console.log(advice);
-  console.log(consultant_notes);
-  console.log(doctorid);
-  console.log(presid);
-
-  const response = await instance.post('/api/updateprescription', {
-    status: 1,
-    diagnosis: diagnosis,
-    advice: advice,
-    consultant_notes: consultant_notes,
-    doctorid: doctorid,
-    presid: presid,
-    meds: meds
-  });
-  
-  console.log(response);
-  closeModal1();
-  closeAddModal();
-}
   return (
-    <div>
-     <label>Enter Your Id </label>
-     <input type="number" id="doctorid" name="doctorid" onChange={(e) => setdoctorid(e.target.value)} />
-     <button onClick={() => { openModal1(); getappointments(); }}>View Appointments</button>
+    <div className={styles.container}>
+      <div className={styles.center}>
+        <div className={styles.centerflex}>
+        <div className={styles.welcomedoctor}>Welcome Doctor</div>
+        {/* <label htmlFor="doctorid">Enter Your Id</label> */}
+        <input className={styles.inputidbox} type="number" id="doctorid" placeholder="Enter ID" name="doctorid" onChange={(e) => setDoctorid(e.target.value)} />
+        <button onClick={() => { openModal1(); getappointments(); }}>View Appointments</button>
+        </div>
+      </div>
 
-     {isModalOpen1 && (
-       <div className="modal">
-       <button onClick={closeModal1}>Close</button>
-       <div>
-           <h1>Appointments</h1>
-   
-           {prescription.length === 0 ? (
-                <>
-                <h3>--------------------------------------------------</h3>
-                <br></br>
-               <h2>         No pending appointments</h2>
-                <br></br>
-                <h3>---------------------------------------------------</h3>
-               </>
-           ) : (
-               <form>
-                   <table>
-                       <thead>
-                           <tr>
-                               <th>Prescription ID</th>
-                               <th>Status</th>
-                               <th>Symptoms</th>
-                           </tr>
-                       </thead>
-                       <tbody>
-                           {prescription.map((prescription) => (
-                               prescription.status === 0 && (
-                                   <tr key={prescription.Pres_ID}>
-                                       <td>{prescription.Pres_ID}</td>
-                                       <td>{prescription.status}</td>
-                                       <td>{prescription.symptoms}</td>
-                                       <td><button onClick={(event) => openAddModal(event, prescription.Pres_ID)}>ADD</button></td>
-                                   </tr>
-                               )
-                           ))}
-                       </tbody>
-                   </table>
-               </form>
-           )}
-       </div>
-   </div>
-     )}
+      {isModalOpen1 && (
+  <div className={`${styles.modal} ${styles.centered}`}>
+    <button onClick={closeModal1}>X</button>
+    <div>
+      <h1>Appointments</h1>
+      {prescription.length === 0 ? (
+        <div>
+          <h2>No pending appointments</h2>
+        </div>
+      ) : (
+        <table className={styles.appointmentsTable}>
+          <thead>
+            <tr>
+              <th>Prescription ID</th>
+              <th>Status</th>
+              <th>Symptoms</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {prescription.map((prescription) => (
+              prescription.status === 0 && (
+                <tr key={prescription.Pres_ID}>
+                  <td>{prescription.Pres_ID}</td>
+                  <td>{prescription.status}</td>
+                  <td>{prescription.symptoms}</td>
+                  <td><button onClick={(event) => openAddModal(event, prescription.Pres_ID)}>ADD</button></td>
+                </tr>
+              )
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  </div>
+)}
+
 
 {isAddModalOpen && (
-        <div className="modal">
-          <button onClick={closeAddModal}>Close</button>
-          <div>
-            <h1>Add Diagnosis and Advice</h1>
+  <div className={`${styles.modal} ${styles.centered}`}>
+    <button className={styles.closeButton} onClick={closeAddModal}>X</button>
+    <div>
+      <h1>Add Diagnosis and Advice</h1>
 
-            <form>
-              <label>
-                Diagnosis:
-                <input onChange={(event)=>setdiagnosis(event.target.value)} type="text" name="diagnosis" />
-              </label>
-              <label>
-                Advice:
-                <input onChange={(event)=>setadvice(event.target.value)} type="text" name="advice" />
-              </label>
-              <label>
-                Consultant Notes:
-                <input onChange={(event)=>setconsultant_notes(event.target.value)} type="text" name="consultant_notes" /> 
-              </label>
-              <button onClick={(event) => 
-                {event.preventDefault();
-                  setIsFormVisible(true)}}>Add Meds</button>
-              {isFormVisible && (
-                  <>
-                    <label>
-                      Medicine Name:
-                      <input id="inpname" onChange={(event) => setCurrentMed({ ...currentMed, name: event.target.value })} type="text" name="name" />
-                    </label>
-                    <label>
-                      Number of Days:
-                      <input id="inpdays" onChange={(event) => setCurrentMed({ ...currentMed, noofdays: event.target.value })} type="number" name="noofdays" />
-                    </label>
-                    <label>
-                      Frequency:
-                      <input id="intimes" onChange={(event) => setCurrentMed({ ...currentMed, frequency: event.target.value })} type="text" name="frequency" />
-                    </label>
-                    <button onClick={(event) => {
+      <form className={styles.form}>
+        <label className={styles.formLabel}>
+          Diagnosis:
+          <input className={styles.input} onChange={(event) => setDiagnosis(event.target.value)} type="text" name="diagnosis" />
+        </label>
+        <label className={styles.formLabel}>
+          Advice:
+          <input className={styles.input} onChange={(event) => setAdvice(event.target.value)} type="text" name="advice" />
+        </label>
+        <label className={styles.formLabel}>
+          Consultant Notes:
+          <input className={styles.input} onChange={(event) => setConsultantNotes(event.target.value)} type="text" name="consultant_notes" />
+        </label>
+        <button className={styles.addButton} onClick={(event) => {
+          event.preventDefault();
+          setIsFormVisible(true);
+        }}>Add Meds</button>
+
+        {isFormVisible && (
+          <>
+            <table className={styles.medsTable}>
+              <thead>
+                {meds.length > 0 && (
+                  <tr>
+                    <th>Medicine Name</th>
+                    <th>Number of Days</th>
+                    <th>Frequency</th>
+                    <th>Action</th>
+                  </tr>
+                )}
+              </thead>
+              <tbody>
+                <tr>
+                  <td>
+                    <input className={styles.input} id="inpname" onChange={(event) => setCurrentMed({ ...currentMed, name: event.target.value })} type="text" name="name" />
+                  </td>
+                  <td>
+                    <input className={styles.input} id="inpdays" onChange={(event) => setCurrentMed({ ...currentMed, noofdays: event.target.value })} type="number" name="noofdays" />
+                  </td>
+                  <td>
+                    <input className={styles.input} id="intimes" onChange={(event) => setCurrentMed({ ...currentMed, frequency: event.target.value })} type="text" name="frequency" />
+                  </td>
+                  <td>
+                    <button className={styles.addButton} onClick={(event) => {
                       event.preventDefault();
                       setMeds(prevMeds => [...prevMeds, currentMed]);
                       setCurrentMed({ name: '', noofdays: '', frequency: '' });
                       document.getElementById('inpname').value = '';
                       document.getElementById('inpdays').value = '';
                       document.getElementById('intimes').value = '';
-
                     }}>Add Med</button>
-                    <br></br>
+                  </td>
+                </tr>
+                {meds.length > 0 && (
+                  <>
+                    {meds.map((med, index) => (
+                      <tr key={index}>
+                        <td>{med.name}</td>
+                        <td>{med.noofdays}</td>
+                        <td>{med.frequency}</td>
+                      </tr>
+                    ))}
+                  </>
+                )}
+              </tbody>
+            </table>
+          </>
+        )}
 
-                  {meds.length > 0 && (
-                     <>
-                     <br></br>
-                        <table>
-                          <thead>
-                            
-                            <tr>
-                              <th>Medicine Name</th>
-                              <th>Number of Days</th>
-                              <th>Frequency</th>
-                            </tr>
-                          </thead>
-                          <tbody>
-                            {meds.map((med, index) => (
-                              <tr key={index}>
-                                <td>{med.name}</td>
-                                <td>{med.noofdays}</td>
-                                <td>{med.frequency}</td>
-                              </tr>
-                            ))}
-                          </tbody>
-                        </table>
-                        </>
-                      )}
-                    </>
-                  )}
+        <button className={styles.submitButton} onClick={(event) => submitUpdatedRecords(event, presIdToUpdate)} type="submit">Submit</button>
+      </form>
+    </div>
+  </div>
+)}
 
-              <button onClick={(event) => submitupdatedrecords(event, presIdToUpdate)} type="submit">Submit</button>
-            </form>
-          </div>
-        </div>
-      )}
-   </div>
-)
+    </div>
+  )
 }
+
 export default DoctorHome;
-
-
-
-
